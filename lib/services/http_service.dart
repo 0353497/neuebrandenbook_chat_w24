4640821a0599b0450e97affe671768d0ff5fa9ba
Niek Geerligs <geerligsniek@gmail.com>
2026-06-13 15:40:39 +0200

    added conversation stuff

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:neuebrandenbook_chat/models/message_conversation_item.dart';
 import 'package:neuebrandenbook_chat/models/room_join_item.dart';
 import 'package:neuebrandenbook_chat/models/room_list_item.dart';
+import 'package:neuebrandenbook_chat/models/user_conversation_item.dart';
 
 class HttpService {
   static String baseUrl = "http://10.0.2.2:3000";
@@ -56,6 +58,24 @@ class HttpService {
   static Future<void> join(String roomId) async {
     try {
       final res = await http.post(Uri.parse("$baseUrl/rooms/$roomId/join"));
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  static Future<List> getConversation(String roomId) async {
+    try {
+      final res = await http.get(Uri.parse("$baseUrl/conversations/$roomId"));
+      final dynamic json = jsonDecode(res.body);
+      final List users = json["users"];
+      final List<UserConversationItem> usersList = users
+          .map((e) => UserConversationItem.fromJson(e))
+          .toList();
+      final List messages = json["messages"];
+      final List<MessageConversationItem> messagesList = messages
+          .map((e) => MessageConversationItem.fromJson(e))
+          .toList();
+      return [users, messagesList];
     } catch (e) {
       rethrow;
     }
