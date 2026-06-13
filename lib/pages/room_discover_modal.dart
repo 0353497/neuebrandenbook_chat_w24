704fd5aab517b338bdaaf8f1a184f6d7962e44ad
Niek Geerligs <geerligsniek@gmail.com>
2026-06-13 14:25:27 +0200

    added some pages

--- lib/pages/room_discover_modal.dart
+++ lib/pages/room_discover_modal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/instance_manager.dart';
+import 'package:get/route_manager.dart';
 
 class RoomDiscoverModal extends StatefulWidget {
   const RoomDiscoverModal({super.key});
@@ -10,6 +12,110 @@ class RoomDiscoverModal extends StatefulWidget {
 class _RoomDiscoverModalState extends State<RoomDiscoverModal> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Dialog(
+      child: SizedBox(
+        width: Get.width,
+        height: Get.height * .8,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 12,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(),
+                  Text(
+                    "Discover Rooms",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  IconButton(
+                    onPressed: () => Get.back(),
+                    icon: Icon(Icons.close),
+                  ),
+                ],
+              ),
+              Text(
+                "“Join book clubs and chat with other readers and authors",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey),
+              ),
+              SearchBar(
+                elevation: WidgetStatePropertyAll(1),
+                leading: Icon(Icons.search, color: Colors.grey),
+                hintText: "Search for rooms...",
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      width: double.maxFinite,
+                      height: 160,
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [CircleAvatar()],
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Title",
+                                        style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        "$index members",
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                      Text(
+                                        "$index descriptions",
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor: WidgetStatePropertyAll(
+                                        Colors.purple,
+                                      ),
+                                      foregroundColor: WidgetStatePropertyAll(
+                                        Colors.black,
+                                      ),
+                                    ),
+                                    onPressed: () {},
+                                    child: Text("Join"),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/room_list_page.dart
+++ lib/pages/room_list_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:neuebrandenbook_chat/pages/room_discover_modal.dart';
 
 class RoomListPage extends StatefulWidget {
   const RoomListPage({super.key});
@@ -10,6 +13,66 @@ class RoomListPage extends StatefulWidget {
 class _RoomListPageState extends State<RoomListPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "NeuBrandenBook",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            Text(
+              "Book Club Chats",
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.purple),
+              foregroundColor: WidgetStatePropertyAll(Colors.white),
+            ),
+            tooltip: "Discover Rooms",
+            onPressed: () => Get.dialog(RoomDiscoverModal()),
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return SizedBox(
+                height: 100,
+                width: Get.width,
+                child: PageView(
+                  controller: PageController(initialPage: 1),
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: Text("Leave")),
+                    ListTile(
+                      leading: Badge.count(
+                        backgroundColor: Colors.purple,
+                        count: 1,
+                        child: CircleAvatar(),
+                      ),
+                      title: Text("Fantasy $index"),
+                      subtitle: Text("Subtitle , $index"),
+                      trailing: Text(
+                        DateFormat("dd/MM/yyyy").format(DateTime.now()),
+                      ),
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text("Pin")),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
